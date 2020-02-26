b901a62e86e22268e3436da2cdf6067feb18ce29
SuzukiTomoya12 <a1220suzuki@gmail.com>
2020-02-26 15:45:14 +0900

    reports-db-migrate

--- db/migrate/20200226053930_create_reports.rb
+++ db/migrate/20200226053930_create_reports.rb
@@ -2,7 +2,7 @@ class CreateReports < ActiveRecord::Migration[5.0]
   def change
     create_table :reports do |t|
 
-      t.string tagname, null: false
+      t.string :tagname, null: false
       t.integer :total_time_hour, null: false
       t.integer :total_time_minute, null: false
       t.integer :concentration_time_hour, null: false
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200224050559) do
+ActiveRecord::Schema.define(version: 20200226053930) do
+
+  create_table "reports", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "tagname",                                 null: false
+    t.integer  "total_time_hour",                         null: false
+    t.integer  "total_time_minute",                       null: false
+    t.integer  "concentration_time_hour",                 null: false
+    t.integer  "concentration_time_minute",               null: false
+    t.text     "content",                   limit: 65535
+    t.text     "image",                     limit: 65535
+    t.integer  "user_id",                                 null: false
+    t.datetime "created_at",                              null: false
+    t.datetime "updated_at",                              null: false
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "username",                            null: false
