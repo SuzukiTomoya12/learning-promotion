e980fb246a902102a32e46dbd5521146637137ac
SuzukiTomoya12 <a1220suzuki@gmail.com>
2020-02-26 20:08:49 +0900

    tags-routing-and-controller-configuration
    # what
    タグのルーティングとコントローラを設定
    仮でタグの登録を行えるページを作成
    
    # why
    レポート作成に伴い、タグを付けられるようにするため。

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,2 +1,13 @@
 class TagsController < ApplicationController
+
+  def index
+  end
+
+  def new
+    @tag = Tag.new
+  end
+
+  def create
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   root to: "tops#index"
   devise_for :users
-  resources :reports, only: [:index, :new, :create]
+
   resources :users, only: [:show]
+  resources :reports, only: [:index, :new, :create]
+  resources :tags, only: [:index, :new, :create]
 end
