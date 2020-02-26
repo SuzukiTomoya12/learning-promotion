bbb7ea026dbb3a7b0824a5ed2a53b4263d891e02
SuzukiTomoya12 <a1220suzuki@gmail.com>
2020-02-26 15:35:34 +0900

    migrate-reports-update

--- db/migrate/20200226053930_create_reports.rb
+++ db/migrate/20200226053930_create_reports.rb
@@ -2,14 +2,14 @@ class CreateReports < ActiveRecord::Migration[5.0]
   def change
     create_table :reports do |t|
 
-      t.string tagname
-      t.integer :total_time_hour
-      t.integer :total_time_minute
-      t.integer :concentration_time_hour
-      t.integer :concentration_time_minute
+      t.string tagname, null: false
+      t.integer :total_time_hour, null: false
+      t.integer :total_time_minute, null: false
+      t.integer :concentration_time_hour, null: false
+      t.integer :concentration_time_minute, null: false
       t.text :content
       t.text :image
-      t.integer :user_id
+      t.integer :user_id, null: false
       t.timestamps
     end
   end
