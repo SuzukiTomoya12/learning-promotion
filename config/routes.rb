a2915414d6f2d71cf75b10e52b481f548ff54118
SuzukiTomoya12 <a1220suzuki@gmail.com>
2020-03-22 16:57:37 +0900

    society-page
    # what
    ユーザー別の新規登録の案内（社会人用）を作成
    
    # why
    ユーザーのタイプによって異なるこのアプリの利用目的を伝えるため

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,16 @@ Rails.application.routes.draw do
 
   root to: "tops#index"
   devise_for :users
+  
+  resources :tops, only: :index do
+    collection do
+      get :society, to: 'tops#society'
+    end
+  end
 
   resources :users, only: [:show]
   resources :reports, only: [:index, :new, :create]
   resources :tags, only: [:index, :new, :create]
+
 
 end
