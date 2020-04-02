3b83dfa25ae1d7eee113ea0afd087d1a1a7c5cf9
SuzukiTomoya12 <a1220suzuki@gmail.com>
2020-04-03 01:10:16 +0900

    db-seed-tags
    # what
    seedファイルでDBにタグを登録するためのコーディングを行った。
    
    # why
    レポート作成時にタグを選択できるようにするため。

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,8 +1,8 @@
 class Tag < ApplicationRecord
 
+  has_ancestry
   has_many :report_tags
   has_many :reports, through: :report_tags
-  belongs_to :parent_tag
 
   # 存在しないと保存できないバリデーション カリキュラム/2678
   validates :name, presence: true, uniqueness: true
