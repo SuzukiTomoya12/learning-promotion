b4afb46675ac65bbee346c074453b2c6a7dbb485
SuzukiTomoya12 <a1220suzuki@gmail.com>
2020-04-16 16:50:30 +0900

    update-exp-table(lv.31~50)
    # what
    経験値テーブルの31~50までをseedファイル上で完成させた

--- db/seeds/experience_point.rb
+++ db/seeds/experience_point.rb
@@ -1,31 +1,86 @@
 # 1時間あたりの獲得経験値量
 @standard_value = 120
-# Lv.21〜50時の計算用(0.24)
-@second_value = @standard_value.to_f / 500
+
+def first_exp_calc(i)
+  @result = (@standard_value * (0.15 - (i.to_f/400)).to_f * i).ceil
+end
+
+def first_total_calc(i)
+  @total = 0
+  @first_total = 0
+  (0..(i-1)).each do |i|
+    first_exp_calc(i)
+    @total += @result
+    if i == (30-1)
+      @first_total = @total
+    end
+  end
+end
+
+def second_exp_calc(i)
+  @result = (270 + ((i-30) **2 * (2.to_f/3)).to_f).ceil
+end
+
+def second_total_calc(i)
+  @total = 0
+  @total += @first_total
+  @second_total = 0
+  (30..(i-1)).each do |i|
+    second_exp_calc(i)
+    @total += @result
+    if i == (50-1)
+      @second_total = @total
+    end
+  end
+end
+
+def third_exp_calc(i)
+  @result = ().ceil
+end
+
+def third_total_calc(i)
+  @total = 0
+  @total += @second_total
+  @third_total = 0
+  (50..(i-1)).each do |i|
+    second_exp_calc(i)
+    @total += @result
+    if i == (50-1)
+      @third_total = @total
+    end
+  end
+end
 
 experience_point_table = []
 def register_data(experience_point_table)
-  # Lv.1〜20
-  (1..20).each do |i|
+  # Lv.1〜30
+  (1..30).each do |i|
     record = {}
-    until_next_level = (@standard_value * (0.15 - (i.to_f/400)).to_f * i).ceil
-    record[:until_next_level] = until_next_level
-
-    total_required_experience = 0
-    (0..(i-1)).each do |i|
-      result = (@standard_value * (0.15 - (i.to_f/400)).to_f * i).ceil
-      total_required_experience += result
-    end
-    record[:total_required_experience] = total_required_experience
+    first_exp_calc(i)
+    record[:until_next_level] = @result
+    first_total_calc(i)
+    record[:total_required_experience] = @total
     experience_point_table << record
   end
 
-  # Lv.21〜50
-  total_so_far = (1..20).sum * @standard_value / 5
-  (21..50).each do |i|
+  # Lv.31〜50
+  (31..50).each do |i|
     record = {}
-    record[:until_next_level] = ((@standard_value * (@second_value - (i/500).to_f)).to_f * i).floor(0).to_i
-    record[:total_required_experience] = total_so_far + (1..(i-1)).sum * 24
+    second_exp_calc(i)
+    record[:until_next_level] = @result
+    second_total_calc(i)
+    record[:total_required_experience] = @total
+    experience_point_table << record
+  end
+  puts "--ここからlv51~100--"
+
+  # Lv.51〜100
+  (51..100).each do |i|
+    record = {}
+    third_exp_calc(i)
+    record[:until_next_level] = @result
+    third_total_calc(i)
+    record[:total_required_experience] = @total
     experience_point_table << record
   end
 end
